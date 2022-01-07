d1e4b29654c5a59ff2b86c645cab1a01efc88f13
joel alcantara burgos <alcantara0729@gmail.com>
2022-01-06 19:49:49 -0500

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by joel Alcantara on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
